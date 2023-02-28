934d818fca6f2321823ef90502714bded1d719cb
Rick Schultz <rick-schultz@hotmail.com>
2023-02-28 01:03:01 +0100

    feat: created simple store

---
+++ stores/counter.ts
@@ -0,0 +1,69 @@
+import { ref } from 'vue'
+import { defineStore } from 'pinia'
+import axios from 'axios'
+
+const baseURL = 'https://pokeapi.co/api/v2/'
+
+interface CharactersProps {
+  name: string
+  url: string
+  stats: {
+    base_stat: number,
+    stat: {
+      name: string
+    }
+  }[]
+  sprites: {
+    other: {
+      dream_world: {
+        front_default: string
+      }
+    }
+  }
+}
+
+export const useCharacterStore = defineStore('character', () => {
+  const characters = ref<CharactersProps[]>([])
+
+  const character = ref<CharactersProps>({} as CharactersProps)
+
+  async function getCharacters() {
+    try {
+      const character = await axios.get(baseURL + 'pokemon?limit=20&offset=0')
+      characters.value = character.data.results
+    }
+    catch (error) {
+      characters.value = []
+    }
+  }
+
+  async function getCharacter(value: string) {
+    try {
+      if (value === '') {
+        getCharacters()
+      } else {
+        const character = await axios.get(baseURL + 'pokemon/' + value)
+        characters.value = character.data
+      }
+    }
+    catch (error) {
+      characters.value = []
+    }
+  }
+
+  async function getCharacterByName(value: string) {
+    try {
+      if (value === '') {
+        getCharacters()
+      } else {
+        const pokemon = await axios.get(baseURL + 'pokemon/' + value)
+        character.value = pokemon.data
+      }
+    }
+    catch (error) {
+      character.value = {} as CharactersProps
+    }
+  }
+
+  return { character, characters, getCharacters, getCharacter, getCharacterByName }
+})
