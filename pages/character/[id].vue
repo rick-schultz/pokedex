9f3f783981c86c38989c77bc37cce27ddee426d3
Rick Schultz <rick-schultz@hotmail.com>
2023-02-28 01:03:21 +0100

    feat: created main page and character page

---
+++ pages/character/[id].vue
@@ -0,0 +1,50 @@
+<script setup lang="ts">
+import { useRoute } from 'vue-router'
+import { useCharacterStore } from '@/stores/counter';
+
+const store = useCharacterStore()
+
+const route = useRoute()
+
+store.getCharacterByName(route.params.id as string)
+</script>
+
+<template>
+  <div class="container">
+    <div class="list">
+      <div class="list">
+        <img
+          :src="store.character.sprites.other.dream_world.front_default"
+          :alt="store.character.name"
+        >
+        <p>Name: {{ store.character.name }}</p>
+        <p
+          v-for="item in store.character.stats"
+          :key="item.stat.name"
+        >
+          {{ item.stat.name }}: {{ item.base_stat }}
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<style lang="scss" scoped>
+.container {
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  .list {
+    margin-top: 1rem;
+    margin-bottom: 1rem;
+  }
+
+  img {
+    width: 150px;
+    height: 150px;
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,77 @@
 <script setup lang="ts">
+import { ref } from 'vue'
+import { useCharacterStore } from '@/stores/counter';
 
+const input = ref()
+const store = useCharacterStore()
+
+store.getCharacters()
+
+const clearCharacters = () => {
+  input.value = ''
+  store.getCharacters()
+}
 </script>
 
 <template>
-  <div>
-    <h1>
-      Homepage
-    </h1>
+  <div class="container">
+    <div class="main">
+      <form>
+        <input
+          id="searchBar"
+          v-model="input"
+          type="text"
+          placeholder="Search for a character"
+          @keyup.enter="store.getCharacter(input)"
+        >
+        <button @click.prevent="store.getCharacter(input)">
+          Search
+        </button>
+        <button @click.prevent="clearCharacters()">
+          Clear
+        </button>
+      </form>
+    </div>
+    <div v-if="store.characters.length === 0">
+      <p>No characters found</p>
+    </div>
+    <div
+      v-for="character in store.characters"
+      :key="character.name"
+    >
+      <div
+        v-if="character.name"
+        class="list"
+      >
+        <NuxtLink :to="`/character/${character.name}`">
+          <p>{{ character.name }}</p>
+          <p>{{ character.url }}</p>
+        </NuxtLink>
+      </div>
+    </div>
   </div>
 </template>
 
 
 <style lang="scss" scoped>
+.container {
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 
+  .main {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    gap: 1rem;
+    margin-top: 2rem;
+    margin-bottom: 2rem;
+  }
+
+  .list {
+    margin-bottom: 1rem;
+  }
+}
 </style>
